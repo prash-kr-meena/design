6e512c54cd872337eb93a0ace8703d5835db155d
prash-kr-meena <social.prash@gmail.com>
2023-10-19 17:17:19 +0530

    Project Setup

---
+++ cricket-match-dashboard/src/main/java/org/meena/CricketMatchDashboardApp.java
@@ -0,0 +1,13 @@
+package org.meena;
+
+/**
+ * Hello world!
+ *
+ */
+public class CricketMatchDashboardApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
