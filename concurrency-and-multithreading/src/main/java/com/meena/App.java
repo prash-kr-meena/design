7b2f03f7780dff8410b78f810d9bddea0962ff8b
prash-kr-meena <social.prash@gmail.com>
2023-10-22 23:06:56 +0530

    concurrency & multithreading

--- concurrency-and-multithreading/src/main/java/com/meena/App.java
+++
@@ -1,9 +0,0 @@
-package com.meena;
-
-public class App {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-
-}
---
+++ concurrency-and-multithreading/src/main/java/com/meena/ConcurrencyAndMultithreading.java
@@ -0,0 +1,9 @@
+package com.meena;
+
+public class ConcurrencyAndMultithreading {
+
+  public static void main(String[] args) {
+    System.out.println("Concurrency And Multithreading - In Java");
+  }
+
+}
